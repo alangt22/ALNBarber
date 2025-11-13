96a8a9aa3f86cb0c4cf35dbafb3a90a6855dc343
alangt22 <alansilva2896@gmail.com>
2025-11-13 18:16:16 -0300

    Corrigido warnings de ESLint e tipos TS

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export const metadata: Metadata = {
 
 interface RootLayoutProps {
   children: React.ReactNode
+   types?: unknown
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
